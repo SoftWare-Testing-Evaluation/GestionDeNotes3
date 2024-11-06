789646a0f3194e0c592c56c45f0b4dd936c1c923
EssiJunior <nessipjunior@gmail.com>
2024-11-06 19:23:52 +0100

    Revert "..."
    This reverts commit 787b86968511ba9b731238ac51953736ba476085.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 
-import Button from './components/Button/Button.jsx'
-
 export default function App() {
     return (
-        <div className='bg-red-400'>
-            Hello world here
-
-            <Button />
-
-        </div>
+        <div className='bg-red-400'>Hello world here</div>
     )
 }
